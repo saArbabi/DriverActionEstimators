d0f9bdcb96620e6420b434ec65629132cee25c39
Salar Arbabi <sa00443@surrey.ac.uk>
2021-04-13 14:34:27 +0100

    Working script

--- models/core/vae.py
+++ models/core/vae.py
@@ -31,12 +31,6 @@ class VAEIDM(AbstractModel):
         self.test_klloss = tf.keras.metrics.Mean(name='train_loss')
         self.test_mseloss = tf.keras.metrics.Mean(name='train_loss')
 
-    def param_activation(self, batch_size, x, min_val, max_val):
-        activation_function = tf.tanh(x)
-        scale = tf.fill([batch_size, 1], (max_val-min_val)/2.)
-        min_val = tf.fill([batch_size, 1], min_val)
-        return tf.add_n([tf.multiply(activation_function, scale), min_val, scale])
-
     # @tf.function
     def sample(self, args):
         z_mean, z_log_sigma = args
@@ -44,19 +38,10 @@ class VAEIDM(AbstractModel):
                                   mean=0., stddev=0.1)
         return z_mean + K.exp(z_log_sigma) * epsilon
 
-    def encode(self, x):
-        mean, logvar = self.encoder(x)
-        return mean, logvar
-
-    def decode(self, z, apply_sigmoid=False):
-        des_v = self.decoder(z)
-        des_v = self.param_activation(tf.shape(z)[0], des_v, 15., 35.)
-        return des_v
-
     def get_actions(self):
-        mean, logvar = self.encode(x)
+        mean, logvar = self.encoder(x)
         z = self.reparameterize(mean, logvar)
-        desired_v = self.decode(z)
+        desired_v = self.decoder(z)
         a_ = self.idm(desired_v)
         return  a_
 
@@ -96,10 +81,10 @@ class VAEIDM(AbstractModel):
         self.test_klloss(kl_loss)
 
     def call(self, inputs):
-        mean, logvar = self.encode(inputs[0])
+        mean, logvar = self.encoder(inputs[0])
         z = self.sample([mean, logvar])
-        des_v = self.decode(z)
-        a_ = self.decoder.idm_sim(inputs[1], des_v)
+        idm_param = self.decoder(z)
+        a_ = self.decoder.idm_sim(inputs[1], idm_param)
         return a_, mean, logvar
 
 class Decoder(tf.keras.Model):
@@ -113,64 +98,70 @@ class Decoder(tf.keras.Model):
         self.layer_2 = Dense(60, activation=K.relu)
         self.layer_3 = Dense(60, activation=K.relu)
         self.layer_4 = Dense(10, activation=K.relu)
-        self.layer_out = Dense(1)
 
-    def idm_sim(self, state, desired_v):
+        self.neu_desired_v = Dense(1)
+        self.neu_desired_tgap = Dense(1, activation=K.exp)
+        self.neu_min_jamx = Dense(1)
+        self.neu_max_act = Dense(1, activation=K.exp)
+        self.neu_min_act = Dense(1, activation=K.exp)
+
+    def param_activation(self, batch_size, x, min_val, max_val):
+        activation_function = tf.tanh(x)
+        scale = tf.fill([batch_size, 1], (max_val-min_val)/2.)
+        min_val = tf.fill([batch_size, 1], min_val)
+        return tf.add_n([tf.multiply(activation_function, scale), min_val, scale])
+
+    def compute_idm_param(self, x, batch_size):
+        desired_v = self.param_activation(batch_size, self.neu_desired_v(x), 15., 35.)
+        min_jamx = tf.abs(self.param_activation(batch_size, self.neu_min_jamx(x), -5., 5.))
+        desired_tgap = self.neu_desired_tgap(x)
+        max_act = self.neu_max_act(x)
+        min_act = self.neu_min_act(x)
+        return [desired_v, desired_tgap, min_jamx, max_act, min_act]
+
+    def idm_sim(self, state, idm_param):
         # state: [v, dv, dx]
-        # if self.model_use == 'training':
-        #     batch_size = 256
-        # elif self.model_use == 'inference' or self.model_use == 'debug':
+        desired_v, desired_tgap, min_jamx, max_act, min_act = idm_param
         batch_size = tf.shape(desired_v)[0]
+        act_seq = tf.zeros([batch_size, 0, 1], dtype=tf.float32)
 
-        # desired_v = self.param_activation(batch_size, self.neu_desired_v(h_t), 15., 35.)
-        desired_tgap = tf.fill([batch_size, 1], 1.5)
-        min_jamx = tf.fill([batch_size, 1], 2.)
-        max_act = tf.fill([batch_size, 1], 1.4)
-        min_act = tf.fill([batch_size, 1], 2.)
+        for step in tf.range(20):
+            tf.autograph.experimental.set_loop_options(shape_invariants=[
+                            (act_seq, tf.TensorShape([None,None,None]))])
 
-        # idm_param = tf.concat([desired_v, desired_tgap, min_jamx, max_act, min_act], axis=1)
+            vel = tf.slice(state, [0, step, 0], [batch_size, 1, 1])
+            dv = tf.slice(state, [0, step, 2], [batch_size, 1, 1])
+            dx = tf.slice(state, [0, step, 3], [batch_size, 1, 1])
+            vel = tf.reshape(vel, [batch_size, 1])
+            dv = tf.reshape(dv, [batch_size, 1])
+            dx = tf.reshape(dx, [batch_size, 1])
 
-        if self.model_use == 'training' or self.model_use == 'debug':
-            act_seq = tf.zeros([batch_size, 0, 1], dtype=tf.float32)
+            mult_1 = tf.multiply(max_act, min_act)
+            mult_2 = tf.multiply(2., tf.sqrt(mult_1))
+            mult_3 = tf.multiply(vel, dv)
+            div_1 = tf.divide(mult_3, mult_2)
+            mult_4 = tf.multiply(desired_tgap, vel)
 
-            for step in tf.range(20):
-                tf.autograph.experimental.set_loop_options(shape_invariants=[
-                                (act_seq, tf.TensorShape([None,None,None]))])
+            desired_gap = tf.add_n([min_jamx, mult_4, div_1])
+            pow_1 = tf.pow(tf.divide(desired_gap, dx), 2.)
+            pow_2 = tf.pow(tf.divide(vel, desired_v), 4.)
+            subtract_1 = tf.add(pow_2, pow_1)
+            subtract_2 = tf.subtract(1., subtract_1)
 
-                vel = tf.slice(state, [0, step, 0], [batch_size, 1, 1])
-                dv = tf.slice(state, [0, step, 2], [batch_size, 1, 1])
-                dx = tf.slice(state, [0, step, 3], [batch_size, 1, 1])
-                vel = tf.reshape(vel, [batch_size, 1])
-                dv = tf.reshape(dv, [batch_size, 1])
-                dx = tf.reshape(dx, [batch_size, 1])
+            act = tf.multiply(max_act, subtract_2)
+            act_seq = tf.concat([act_seq, tf.reshape(act, [batch_size, 1, 1])], axis=1)
 
-                mult_1 = tf.multiply(max_act, min_act)
-                mult_2 = tf.multiply(2., tf.sqrt(mult_1))
-                mult_3 = tf.multiply(vel, dv)
-                div_1 = tf.divide(mult_3, mult_2)
-                mult_4 = tf.multiply(desired_tgap, vel)
-
-                desired_gap = tf.add_n([min_jamx, mult_4, div_1])
-                pow_1 = tf.pow(tf.divide(desired_gap, dx), 2.)
-                pow_2 = tf.pow(tf.divide(vel, desired_v), 4.)
-                subtract_1 = tf.add(pow_2, pow_1)
-                subtract_2 = tf.subtract(1., subtract_1)
-
-                act = tf.multiply(max_act, subtract_2)
-                act_seq = tf.concat([act_seq, tf.reshape(act, [batch_size, 1, 1])], axis=1)
-
-            return act_seq
-            # return act_seq, idm_param
-
-        elif self.model_use == 'inference':
-            return idm_param
+        return act_seq
 
     def call(self, inputs):
         x = self.layer_1(inputs)
         x = self.layer_2(x)
         x = self.layer_3(x)
         x = self.layer_4(x)
-        return self.layer_out(x)
+        batch_size = tf.shape(inputs)[0]
+        idm_param = self.compute_idm_param(x, batch_size)
+
+        return idm_param
 
 class Encoder(tf.keras.Model):
     def __init__(self):
