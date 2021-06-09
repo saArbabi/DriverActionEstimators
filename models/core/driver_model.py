0ffa6fa77d44ccd57b2fd670ee5e010d9ed645df
Salar Arbabi <sa00443@surrey.ac.uk>
2021-06-09 11:32:30 +0100

    -

--- models/core/driver_model.py
+++ models/core/driver_model.py
@@ -220,17 +220,17 @@ class Arbiter(tf.keras.Model):
         self.architecture_def()
 
     def architecture_def(self):
-        self.layer_1 = Dense(100)
-        # self.layer_2 = Dense(50, activation=K.relu)
-        # self.layer_3 = Dense(50, activation=K.relu)
-        # self.layer_4 = Dense(50, activation=K.relu)
+        self.layer_1 = Dense(50, activation=K.relu)
+        self.layer_2 = Dense(50, activation=K.relu)
+        self.layer_3 = Dense(50, activation=K.relu)
+        self.layer_4 = Dense(50, activation=K.relu)
         self.attention_neu = Dense(20)
 
     def call(self, inputs):
         x = self.layer_1(inputs)
-        # x = self.layer_/2(x)
-        # x = self.layer_3(x)
-        # x = self.layer_4(x)
+        x = self.layer_2(x)
+        x = self.layer_3(x)
+        x = self.layer_4(x)
         x = self.attention_neu(x)
         return 1/(1+tf.exp(-self.attention_temp*x))
 
@@ -292,10 +292,10 @@ class IDMLayer(tf.keras.Model):
         self.architecture_def()
 
     def architecture_def(self):
-        self.layer_1 = Dense(100)
-        # self.layer_2 = Dense(50, activation=K.relu)
-        # self.layer_3 = Dense(50, activation=K.relu)
-        # self.layer_4 = Dense(50, activation=K.relu)
+        self.layer_1 = Dense(50, activation=K.relu)
+        self.layer_2 = Dense(50, activation=K.relu)
+        self.layer_3 = Dense(50, activation=K.relu)
+        self.layer_4 = Dense(50, activation=K.relu)
 
         self.des_v_layer = Dense(self.enc_units)
         self.des_v_neu = Dense(1)
@@ -347,6 +347,9 @@ class IDMLayer(tf.keras.Model):
         batch_size = tf.shape(inputs)[0]
 
         x = self.layer_1(inputs)
+        x = self.layer_2(x)
+        x = self.layer_3(x)
+        x = self.layer_4(x)
 
 
         desired_v = self.get_des_v(x, batch_size)
