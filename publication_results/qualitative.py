32a7fe2f2a1187b1dcb8fd3c9ff5cee404f2aff5
Salar Arbabi <sa00443@surrey.ac.uk>
2021-08-30 12:08:40 +0100

    -

--- publication_results/qualitative.py
+++ publication_results/qualitative.py
@@ -15,6 +15,8 @@ plt.style.use(['science','ieee'])
 """
 Driver generation - beta distribution with aggressiveness levels
 """
+# plt.style.use('default')
+
 x = np.linspace(0.0, 1, 100)
 color_i = 0
 range_means = np.linspace(0.01, 0.99, 10)
@@ -22,12 +24,11 @@ colors = [cm.rainbow(i) for i in np.linspace(0, 1, len(range_means))]
 alpha_val_plot = 0.8
 precision = 10
 fig = plt.figure(figsize=(4, 3))
-
+var = 0.03
 for mean in range_means:
-    # mean = 0.3
     # plt.figure((3, 3))
-    alpha_param = mean*precision
-    beta_param = precision*(1-mean)
+    alpha_param = (((1-mean)/var)-1/mean)*mean**2
+    beta_param = alpha_param*((1/mean)-1)
     p = beta.pdf(x, alpha_param, beta_param)
     plt.plot(x, p, color=colors[color_i], linewidth=1)
     plt.fill_between(x, p, color=colors[color_i], alpha=alpha_val_plot)
@@ -38,8 +39,15 @@ for mean in range_means:
     # plt.xlim(0, 1)
     plt.minorticks_off()
     color_i += 1
-plt.savefig("beta_densities.png", dpi=500)
+# plt.savefig("beta_densities.png", dpi=500)
 
 
 
 # %%
+mean = 0.05
+color_i = 0
+var = 0.02
+alpha_param = (((1-mean)/var)-1/mean)*mean**2
+beta_param = alpha_param*((1/mean)-1)
+p = beta.pdf(x, alpha_param, beta_param)
+plt.plot(x*35, p, color=colors[color_i], linewidth=1)
