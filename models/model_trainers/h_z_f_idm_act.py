4f7a9363dd6f7b39fdc608aef783011ff6a00429
Salar Arbabi <sa00443@surrey.ac.uk>
2021-11-09 14:25:22 +0000

    3d latent visualisation

--- models/model_trainers/h_z_f_idm_act.py
+++ models/model_trainers/h_z_f_idm_act.py
@@ -409,36 +409,57 @@ def latent_samples(model_trainer, sample_index):
     return sampled_z
 
 def latent_vis(n_z_samples):
-    fig = plt.figure(figsize=(4, 6))
-    # plt.style.use('ggplot')
-    # plt.style.use('default')
-    ax = fig.add_subplot(211)
+    fig = pyplot.figure(figsize=(4, 6))
     examples_to_vis = np.random.choice(val_examples, n_z_samples, replace=False)
+    #===============
+    #  First subplot
+    #===============
+    # set up the axes for the first plot
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_zticks([])
     sampled_z = latent_samples(model_trainer, examples_to_vis)
     aggressiveness = history_future_usc[examples_to_vis, 0, -7]
     color_shade = aggressiveness
-    att_sc = ax.scatter(sampled_z[:, 0], sampled_z[:, 1],
-                        s=5, c=color_shade, cmap='rainbow', edgecolors='black', linewidth=0.2)
+    att_sc = ax.scatter(sampled_z[:, 0], sampled_z[:, 1], sampled_z[:, 2],
+                  s=5, c=color_shade, cmap='rainbow', edgecolors='black', linewidth=0.2)
 
-
+    ax.tick_params(pad=1)
+    ax.grid(False)
+    # ax.view_init(30, 50)
+    #===============
+    #  Second subplot
+    #===============
+    # set up the axes for the second plot
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_zticks([])
+    att_sc = ax.scatter(sampled_z[:, 0], sampled_z[:, 1], sampled_z[:, 2],
+                  s=5, c=color_shade, cmap='rainbow', edgecolors='black', linewidth=0.2)
 
     axins = inset_axes(ax,
                         width="5%",
-                        height="100%",
+                        height="90%",
                         loc='right',
                         borderpad=-2
                        )
 
     fig.colorbar(att_sc, cax=axins)
     cbar = fig.colorbar(att_sc, cax=axins)
-    cbar.set_label('$\psi$')
-    ax.set_ylabel('$z_{1}$')
-    ax.set_xlabel('$z_{2}$')
-    # ax.set_title('Driver attention')
-    # idm_axis.set_title('Driver disposition')
+    ax.tick_params(pad=1)
+    ax.grid(False)
+    ax.view_init(30, 50)
+    # ax.set_xlabel('$z_{1}$', labelpad=1)
+    # ax.set_ylabel('$z_{2}$', labelpad=1)
+    # ax.set_zlabel('$z_{3}$', labelpad=1)
+    plt.subplots_adjust(wspace=0.2, hspace=None)
 
-    return ax
-ax = latent_vis(2000)
+latent_vis(2000)
+# plt.savefig("latent.png", dpi=500)
+
 
 # %%
 
