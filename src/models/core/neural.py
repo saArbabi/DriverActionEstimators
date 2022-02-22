2fe92fd36fa7a3a4038a3b786155c62a04d17696
Salar Arbabi <sa00443@surrey.ac.uk>
2022-02-22 10:14:04 +0000

    neural_040

--- src/models/core/neural.py
+++ src/models/core/neural.py
@@ -2,16 +2,20 @@
 from tensorflow.keras.layers import Dense, LSTM, Bidirectional, TimeDistributed, LeakyReLU
 from keras import backend as K
 from importlib import reload
-from models.core import abstract_model
-reload(abstract_model)
-from models.core.neural_idm import  AbstractModel
+from models.core import neural_idm
+reload(neural_idm)
+from models.core.neural_idm import  NeurIDMModel
 import tensorflow as tf
 import tensorflow_probability as tfp
 tfd = tfp.distributions
 
 class NeurLatentModel(NeurIDMModel):
-    def __init__(self, config):
-        super(NeurLatentModel, self).__init__(config)
+    def __init__(self, config, exp_id=None):
+        super(NeurLatentModel, self).__init__(config, exp_id)
+        self.belief_net = BeliefModel(config)
+        self.forward_sim = ForwardSim()
+        if exp_id:
+            self.exp_dir = './src/models/experiments/' + 'neural_' + exp_id
 
     def get_kl_loss(self, pri_params, pos_params):
         pri_mean, pri_logsigma = pri_params
@@ -35,7 +39,7 @@ class NeurLatentModel(NeurIDMModel):
 
 
         gradients = tape.gradient(loss, self.trainable_variables)
-        # tf.print('xxxxx gradients: ', gradients)
+        tf.debugging.check_numerics(loss, message='Checking loss')
 
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
         with self.train_writer.as_default():
@@ -67,14 +71,15 @@ class NeurLatentModel(NeurIDMModel):
         pri_params, pos_params = self.belief_net(\
                                 [enc_h, enc_f], dis_type='both')
         sampled_z = self.belief_net.sample_z(pos_params)
-        proj_latent = self.belief_net.belief_proj(sampled_z)
-        displacement_seq, _ = self.forward_sim.rollout([proj_latent, enc_h, inputs[2], inputs[-1]])
-        return displacement_seq, pri_params, pos_params
+        proj_latent = self.belief_net.z_proj(sampled_z)
+        displacement_seq, action_seq = self.forward_sim.rollout([\
+                                    proj_latent, inputs[2], inputs[-1]])
+        return [displacement_seq, action_seq], pri_params, pos_params
 
 class BeliefModel(tf.keras.Model):
     def __init__(self, config):
         super(BeliefModel, self).__init__(name="BeliefModel")
-        self.proj_dim = 64
+        self.proj_dim = 128
         self.latent_dim = config['model_config']['latent_dim']
         self.architecture_def()
 
@@ -83,11 +88,11 @@ class BeliefModel(tf.keras.Model):
         self.pri_logsigma = Dense(self.latent_dim)
         self.pos_mean = Dense(self.latent_dim)
         self.pos_logsigma = Dense(self.latent_dim)
-        self.pri_projection = Dense(self.proj_dim, activation=LeakyReLU())
-        self.pos_projection = Dense(self.proj_dim, activation=LeakyReLU())
-        ####
-        self.proj_layer_1 = Dense(self.proj_dim, activation=LeakyReLU())
-        self.proj_layer_2 = Dense(self.proj_dim, activation=LeakyReLU())
+
+        self.proj_pri = Dense(self.proj_dim, activation=LeakyReLU())
+        self.proj_pos = Dense(self.proj_dim, activation=LeakyReLU())
+
+        self.proj_layer = Dense(self.proj_dim, activation=LeakyReLU())
 
     def sample_z(self, dis_params):
         z_mean, z_logsigma = dis_params
@@ -98,21 +103,25 @@ class BeliefModel(tf.keras.Model):
         # tf.print('z_min: ', tf.reduce_min(z_sigma))
         return sampled_z
 
-    def belief_proj(self, x):
-        x = self.proj_layer_1(x)
-        x = self.proj_layer_2(x)
-        return x
+    def pri_proj(self, enc_h):
+        return self.proj_pri(enc_h)
+
+    def pos_proj(self, enc_h, enc_f):
+        return self.proj_pos(tf.concat([enc_h, enc_f], axis=-1))
+
+    def z_proj(self, x):
+        return self.proj_layer(x)
 
     def call(self, inputs, dis_type):
         if dis_type == 'both':
             enc_h, enc_f = inputs
             # prior
-            pri_context = self.pri_projection(enc_h)
+            pri_context = self.pri_proj(enc_h)
             pri_mean = self.pri_mean(pri_context)
             pri_logsigma = self.pri_logsigma(pri_context)
 
             # posterior
-            pos_context = self.pos_projection(tf.concat([enc_h, enc_f], axis=-1))
+            pos_context = self.pos_proj(enc_h, enc_f)
             pos_mean = self.pos_mean(pos_context)
             pos_logsigma = self.pos_logsigma(pos_context)
 
@@ -121,95 +130,56 @@ class BeliefModel(tf.keras.Model):
             return pri_params, pos_params
 
         elif dis_type == 'prior':
-            pri_context = self.pri_projection(inputs)
+            pri_context = self.pri_proj(inputs)
             pri_mean = self.pri_mean(pri_context)
             pri_logsigma = self.pri_logsigma(pri_context)
             pri_params = [pri_mean, pri_logsigma]
             return pri_params
 
-class HistoryEncoder(tf.keras.Model):
-    def __init__(self):
-        super(HistoryEncoder, self).__init__(name="HistoryEncoder")
-        self.enc_units = 128
-        self.architecture_def()
-
-    def architecture_def(self):
-        self.lstm_layer_1 = LSTM(self.enc_units, return_sequences=True)
-        self.lstm_layer_2 = LSTM(self.enc_units)
-
-    def call(self, inputs):
-        whole_seq_output = self.lstm_layer_1(inputs)
-        enc_h = self.lstm_layer_2(whole_seq_output)
-        return enc_h
-
-class FutureEncoder(tf.keras.Model):
-    def __init__(self):
-        super(FutureEncoder, self).__init__(name="FutureEncoder")
-        self.enc_units = 128
-        self.architecture_def()
-
-    def architecture_def(self):
-        self.lstm_layer = Bidirectional(LSTM(self.enc_units), merge_mode='concat')
-
-    def call(self, inputs):
-        enc_acts = self.lstm_layer(inputs)
-        return enc_acts
 
 class ForwardSim(tf.keras.Model):
     def __init__(self):
         super(ForwardSim, self).__init__(name="ForwardSim")
-        self.proj_dim = 64
+        self.proj_dim = 128
         self.dec_units = 128
         self.architecture_def()
 
     def architecture_def(self):
-        self.dense_1 = TimeDistributed(Dense(self.dec_units, activation=LeakyReLU()))
-        self.dense_2 = TimeDistributed(Dense(self.dec_units, activation=LeakyReLU()))
-        self.dense_3 = TimeDistributed(Dense(self.dec_units, activation=LeakyReLU()))
-        self.action_neu = TimeDistributed(Dense(1))
         self.lstm_layer = LSTM(self.dec_units, return_sequences=True, return_state=True)
+        self.att_layer_1 = TimeDistributed(Dense(self.dec_units, activation=LeakyReLU()))
+        self.att_layer_2 = TimeDistributed(Dense(self.dec_units, activation=LeakyReLU()))
+        self.action_neu = TimeDistributed(Dense(1))
 
     def scale_env_s(self, env_state):
         env_state = (env_state-self.env_scaler.mean_)/self.env_scaler.var_**0.5
         return env_state
 
-    def get_action(self, inputs, lstm_states):
-        lstm_output, state_h, state_c = self.lstm_layer(inputs, initial_state=lstm_states)
-        x = self.dense_1(lstm_output)
-        x = self.dense_2(x)
-        x = self.dense_3(x)
-        return self.action_neu(x), [state_h, state_c]
+    def get_action(self, inputs):
+        x = self.att_layer_1(inputs)
+        x = self.att_layer_2(x)
+        return self.action_neu(x)
 
     def rollout(self, inputs):
-        proj_latent, enc_h, idm_s, merger_cs = inputs
+        proj_latent, idm_s, merger_cs = inputs
         batch_size = tf.shape(idm_s)[0]
         proj_latent = tf.reshape(proj_latent, [batch_size, 1, self.proj_dim])
-        enc_h = tf.reshape(enc_h, [batch_size, 1, self.dec_units])
-        init_lstm_state = tf.zeros([batch_size, self.dec_units])
-        lstm_states = [init_lstm_state, init_lstm_state]
 
-        for step in range(self.rollout_len):
-            f_veh_v = idm_s[:, step:step+1, 1:2]
-            m_veh_v = idm_s[:, step:step+1, 2:3]
-            f_veh_glob_x = idm_s[:, step:step+1, 4:5]
-            m_veh_glob_x = idm_s[:, step:step+1, 5:6]
+        displacement = tf.zeros([batch_size, 1, 1])
+        displacement_seq = displacement
+        ego_v = idm_s[:,  0:1, 0:1]
+        ego_glob_x = idm_s[:,  0:1, 3:4]
+        lstm_output = self.lstm_layer(ego_glob_x)
+        for step in range(1, self.rollout_len+1):
+            f_veh_v = idm_s[:, step-1:step, 1:2]
+            m_veh_v = idm_s[:, step-1:step, 2:3]
+            f_veh_glob_x = idm_s[:, step-1:step, 4:5]
+            m_veh_glob_x = idm_s[:, step-1:step, 5:6]
 
-            ef_dv_true = idm_s[:, step:step+1, 6:7]
-            ef_delta_x_true = idm_s[:, step:step+1, 7:8]
-            em_dv_true = idm_s[:, step:step+1, 8:9]
-            em_delta_x_true = idm_s[:, step:step+1, 9:10]
+            em_dv_true = idm_s[:, step-1:step, 8:9]
+            em_delta_x_true = idm_s[:, step-1:step, 9:10]
 
             # these to deal with missing cars
-            m_veh_exists = idm_s[:, step:step+1, -1:]
-            if step == 0:
-                ego_v = idm_s[:, step:step+1, 0:1]
-                ego_glob_x = idm_s[:, step:step+1, 3:4]
-                displacement = tf.zeros([batch_size, 1, 1])
-            else:
-                ego_v += _act*0.1
-                ego_glob_x += ego_v*0.1 + 0.5*_act*0.1**2
-                displacement += ego_v*0.1 + 0.5*_act*0.1**2
-
+            m_veh_exists = idm_s[:, step-1:step, -1:]
 
             ef_delta_x = (f_veh_glob_x - ego_glob_x)
             em_delta_x = (m_veh_glob_x - ego_glob_x)*m_veh_exists+\
@@ -221,16 +191,19 @@ class ForwardSim(tf.keras.Model):
             env_state = tf.concat([ego_v, f_veh_v, \
                                     ef_dv, ef_delta_x, em_dv, em_delta_x], axis=-1)
             env_state = self.scale_env_s(env_state)
-            merger_c = merger_cs[:, step:step+1, :]
-            inputs = tf.concat([proj_latent, enc_h, env_state, merger_c], axis=-1)
+            merger_c = merger_cs[:, step-1:step, :]
+            inputs = tf.concat([proj_latent, env_state, merger_c], axis=-1)
 
-            _act, lstm_states = self.get_action(inputs, lstm_states)
-            if step == 0:
+            _act = self.get_action(inputs)
+
+            displacement += ego_v*0.1 + 0.5*_act*0.1**2
+            ego_glob_x += ego_v*0.1 + 0.5*_act*0.1**2
+            ego_v += _act*0.1
+            if step-1 == 0:
                 act_seq = _act
-                displacement_seq = displacement
 
             else:
                 act_seq = tf.concat([act_seq, _act], axis=1)
-                displacement_seq = tf.concat([displacement_seq, displacement], axis=1)
+            displacement_seq = tf.concat([displacement_seq, displacement], axis=1)
 
         return displacement_seq, act_seq
