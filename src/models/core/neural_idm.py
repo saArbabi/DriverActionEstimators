e2c295830be275e78d4e3e3fafb10e05f4e63778
Salar Arbabi <sa00443@surrey.ac.uk>
2022-02-23 12:59:14 +0000

    neural_idm_325

--- src/models/core/neural_idm.py
+++ src/models/core/neural_idm.py
@@ -125,7 +125,7 @@ class NeurIDMModel(AbstractModel):
         proj_att = self.belief_net.z_proj_att(z_att)
         idm_params = self.idm_layer(proj_idm)
         displacement_seq, action_seq, _ = self.forward_sim.rollout([\
-                                idm_params, proj_att,
+                                idm_params, proj_att, enc_h,
                                 inputs[2], inputs[-1]])
         # tf.print('###############:')
         # tf.print('att_scoreax: ', tf.reduce_max(att_scores))
@@ -275,8 +275,10 @@ class IDMForwardSim(tf.keras.Model):
     def get_att(self, inputs, step):
         x = self.att_layer_1(inputs)
         x = self.att_layer_2(x)
-        # f_att_x = self.clip_value(self.f_att_neu(x), 100)
-        # m_att_x = self.clip_value(self.m_att_neu(x), 100)
+        f_att_x = self.clip_value(self.f_att_neu(x), 20)
+        m_att_x = self.clip_value(self.m_att_neu(x), 20)
+        # f_att_x = self.f_att_neu(x)
+        # m_att_x = self.m_att_neu(x)
         # if step == 10:
         #     tf.print('xxxxx f_att_x  min: ', tf.reduce_min(f_att_x))
         #     tf.print('xxxxx f_att_x  max: ', tf.reduce_max(f_att_x))
@@ -294,21 +296,25 @@ class IDMForwardSim(tf.keras.Model):
         return tf.split(idm_params, 5, axis=-1)
 
     def rollout(self, inputs):
-        idm_params, proj_latent, idm_s, merger_cs = inputs
+        idm_params, proj_latent, enc_h, idm_s, merger_cs = inputs
         batch_size = tf.shape(idm_s)[0]
         idm_params = self.reshape_idm_params(idm_params, batch_size)
         proj_latent = tf.reshape(proj_latent, [batch_size, 1, self.proj_dim])
+        enc_h = tf.reshape(enc_h, [batch_size, 1, self.dec_units])
 
         displacement = tf.zeros([batch_size, 1, 1])
         displacement_seq = displacement
         ego_v = idm_s[:,  0:1, 0:1]
+        ego_veh_a = idm_s[:, 0:1, 11:12]
         ego_glob_x = idm_s[:,  0:1, 3:4]
         lstm_output = self.lstm_layer(ego_glob_x)
+
         for step in range(1, self.rollout_len+1):
             f_veh_v = idm_s[:, step-1:step, 1:2]
             m_veh_v = idm_s[:, step-1:step, 2:3]
             f_veh_glob_x = idm_s[:, step-1:step, 4:5]
             m_veh_glob_x = idm_s[:, step-1:step, 5:6]
+            f_veh_a = idm_s[:, step-1:step, 12:13]
 
             em_dv_true = idm_s[:, step-1:step, 8:9]
             em_delta_x_true = idm_s[:, step-1:step, 9:10]
@@ -323,12 +329,13 @@ class IDMForwardSim(tf.keras.Model):
             em_dv = (ego_v - m_veh_v)*m_veh_exists+\
                             (1-m_veh_exists)*self.dummy_value_set['em_delta_v']
             # tf.print('############ ef_act ############')
-            env_state = tf.concat([ego_v, f_veh_v, \
+
+            env_state = tf.concat([ego_v, f_veh_v, ego_veh_a, f_veh_a,\
                                     ef_dv, ef_delta_x, em_dv, em_delta_x], axis=-1)
             env_state = self.scale_env_s(env_state)
             merger_c = merger_cs[:, step-1:step, :]
 
-            inputs = tf.concat([proj_latent, env_state, merger_c], axis=-1)
+            inputs = tf.concat([proj_latent, enc_h, env_state, merger_c], axis=-1)
             f_att_score, m_att_score = self.get_att(inputs, step)
             # m_att_score = idm_s[:, step-1:step, -3:-2]
             # f_att_score = 1 - m_att_score
@@ -337,20 +344,21 @@ class IDMForwardSim(tf.keras.Model):
             idm_state = [ego_v, em_dv, em_delta_x]
             em_act = self.idm_driver(idm_state, idm_params)
             # _act = f_att_score*ef_act + m_att_score*em_act
-            _act = f_att_score*ef_act + m_att_score*em_act
+            ego_veh_a = f_att_score*ef_act + \
+                    m_att_score*em_act*tf.cast(tf.greater(em_delta_x, 0), tf.float32)*m_veh_exists
 
-            displacement += ego_v*0.1 + 0.5*_act*0.1**2
-            ego_glob_x += ego_v*0.1 + 0.5*_act*0.1**2
-            ego_v += _act*0.1
+            displacement += ego_v*0.1 + 0.5*ego_veh_a*0.1**2
+            ego_glob_x += ego_v*0.1 + 0.5*ego_veh_a*0.1**2
+            ego_v += ego_veh_a*0.1
 
             if step-1 == 0:
                 # first step
-                act_seq = _act
+                act_seq = ego_veh_a
                 f_att_seq = f_att_score
                 m_att_seq = m_att_score
 
             else:
-                act_seq = tf.concat([act_seq, _act], axis=1)
+                act_seq = tf.concat([act_seq, ego_veh_a], axis=1)
                 f_att_seq = tf.concat([f_att_seq, f_att_score], axis=1)
                 m_att_seq = tf.concat([m_att_seq, m_att_score], axis=1)
 
